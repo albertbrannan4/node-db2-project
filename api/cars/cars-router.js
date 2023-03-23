f6a2ad2120ac5cec0cbe80d72dcb48eb89532079
Albert Brannan <89121878+albertbrannan4@users.noreply.github.com>
2023-03-23 17:25:51 -0400

    router created

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,6 @@
 // DO YOUR MAGIC
+const router = require("express").Router();
+router.get("/", (req, res) => {
+  res.json("endpoint works");
+});
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
-const express = require("express")
-
-const server = express()
-
+const express = require("express");
+const carRouter = require("./cars/cars-router");
+const server = express();
+server.use("/api/cars", carRouter);
 // DO YOUR MAGIC
 
-module.exports = server
+module.exports = server;
