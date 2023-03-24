c9e348f138c1317ebb00997c290ca97655d6db24
Albert Brannan <89121878+albertbrannan4@users.noreply.github.com>
2023-03-24 17:21:22 -0400

    check if id exists middleware and created post endpoint

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,15 +1,27 @@
-const checkCarId = (req, res, next) => {
+const { getById } = require("./cars-model");
+
+const checkCarId = async (req, res, next) => {
   // DO YOUR MAGIC
-}
+  let exists = await getById(req.params.id);
+  if (!exists) {
+    next({ status: 404, message: "id not found" });
+  } else {
+    next();
+  }
+};
 
 const checkCarPayload = (req, res, next) => {
   // DO YOUR MAGIC
-}
+};
 
 const checkVinNumberValid = (req, res, next) => {
   // DO YOUR MAGIC
-}
+};
 
 const checkVinNumberUnique = (req, res, next) => {
   // DO YOUR MAGIC
-}
+};
+
+module.exports = {
+  checkCarId,
+};
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,21 +1,28 @@
 // DO YOUR MAGIC
 const router = require("express").Router();
 const { getAll, getById } = require("./cars-model");
+const { checkCarId } = require("./cars-middleware");
+
 router.get("/", async (req, res) => {
-  try {
-    let allCars = await getAll();
-    res.status(200).json(allCars);
-  } catch (err) {
-    res.status(500).json({ message: "Cannot acquire all cars" });
-  }
+  let allCars = await getAll();
+  res.status(200).json(allCars);
 });
 
-router.get("/:id", async (req, res) => {
+router.get("/:id", checkCarId, async (req, res, next) => {
   try {
     let car = await getById(req.params.id);
     res.status(200).json(car);
   } catch (err) {
-    res.status(500).json({ message: "Cannot acquire car" });
+    next(err);
   }
 });
+
+router.post("/", async (req, res, next) => {
+  res.json("this works");
+});
+
+router.use((err, req, res, next) => {
+  res.status(err.status || 500).json({ message: err.message });
+});
+
 module.exports = router;
